e5a817c193de55851f1b9f9c98601c12e513b722
cj-nassif <nassif.cj@gmail.com>
2022-01-21 07:12:58 -0300

    Personalized TabBottom Navigation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,24 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar, StyleSheet, BackHandler } from 'react-native';
-
-import {
-    RectButton,
-    PanGestureHandler
-
-} from 'react-native-gesture-handler';
+import { StatusBar } from 'react-native';
 
 import { RFValue } from 'react-native-responsive-fontsize';
-import { useTheme } from 'styled-components';
-
-import Animated, {
-    useSharedValue,
-    useAnimatedStyle,
-    useAnimatedGestureHandler,
-    withSpring
-} from 'react-native-reanimated';
-const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
-
-import { Ionicons } from '@expo/vector-icons';
 
 import { api } from '../../services/api';
 
@@ -28,7 +11,6 @@ import {
     TotalCars,
     HeaderContent,
     CarList,
-    MyCarsButton
 } from './styles';
 
 import Logo from '../../assets/logo.svg';
@@ -38,7 +20,6 @@ import { LoadAnimation } from '../../components/LoadAnimation';
 import { useNavigation } from '@react-navigation/native';
 
 import { CarDTO } from '../../dtos/CarDTO';
-import { Inter_600SemiBold } from '@expo-google-fonts/inter';
 
 
 export function Home() {
@@ -48,45 +29,12 @@ export function Home() {
 
     const navigation = useNavigation();
 
-    const theme = useTheme();
-
-    const positionY = useSharedValue(0);
-    const positionX = useSharedValue(0);
-
-    const myCarsButtonStyles = useAnimatedStyle(() => {
-        return {
-            transform: [
-                { translateX: positionX.value },
-                { translateY: positionY.value }
-            ]
-        }
-    })
-
-    const onGestureEvent = useAnimatedGestureHandler({
-        onStart(_, ctx: any) {
-            ctx.positionX = positionX.value;
-            ctx.positionY = positionY.value;
-
-        },
-        onActive(event, ctx: any) {
-            positionX.value = ctx.positionX + event.translationX;
-            positionY.value = ctx.positionY + event.translationY;
-        },
-        onEnd() {
-            positionX.value = withSpring(0);
-            positionY.value = withSpring(0);
-        }
-    });
-
-
 
     function handleCarDetails(car: CarDTO) {
         navigation.navigate('CarDetails' as never, { car } as never)
     }
 
-    function handleOpenMyCars() {
-        navigation.navigate('MyCars' as never)
-    }
+
 
     useEffect(() => {
         async function fetchCars() {
@@ -101,12 +49,6 @@ export function Home() {
             }
         }
         fetchCars();
-    }, [])
-
-    useEffect(() => {
-        BackHandler.addEventListener('hardwareBackPress', () => {
-            return true;
-        })
     }, [])
 
     return (
@@ -144,42 +86,8 @@ export function Home() {
                             />}
                     />
             }
-            <PanGestureHandler onGestureEvent={onGestureEvent}>
-                <Animated.View
-                    style={[
-                        myCarsButtonStyles,
-                        {
-                            position: 'absolute',
-                            bottom: 13,
-                            right: 22
-                        }
-                    ]}
-                >
 
-                    <ButtonAnimated
-                        onPress={handleOpenMyCars}
-                        style={[styles.button, { backgroundColor: theme.colors.main }]}
-                    >
-                        <Ionicons
-                            name='ios-car-sport'
-                            size={32}
-                            color={theme.colors.shape}
-                        />
-                    </ButtonAnimated>
-
-                </Animated.View>
-
-            </PanGestureHandler>
         </Container>
     );
 }
 
-const styles = StyleSheet.create({
-    button: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
